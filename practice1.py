e85bdcb23840db2d3614e86758bebaf4dcd67848
krishnanramasamy <krish96882@gmail.com>
2024-08-24 13:14:41 +0530

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,225 @@
+# print("hello world!")
+# i am harikrishnan, i am from bhavani, i studied from guvi institutions about python automation testing
+
+# language = "python"
+# print(language)
+
+# mynumber = 345
+# print(mynumber)
+
+# mytext = "i am harikrishnan, i am from bhavani, i studied automation from guvi"
+# print(mytext)
+# print(len(mytext))
+
+# myvalue ="i studied from guvi institutions"
+# print(myvalue[2:5])
+# print(myvalue[0:10])
+# print(myvalue[2:7])
+# print(myvalue[:])
+# print(myvalue[:10])
+# print(myvalue[-10:-1])
+# print(myvalue[::-1])
+
+# user_input = "hari"
+# reversed_string = user_input[::-1]
+# print(reversed_string)
+
+# if user_input == reversed_string:
+  #  print("palindrome")
+# else:
+  #  print("not palindrome")    
+
+# x=5.2
+# y=10
+# z=5j
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# x=8j
+# y=5
+# z=5.2
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# a = complex (x)
+# print(type(a)) 
+
+# b = int (y)
+# print(type(b))
+
+# c = float (z)
+# print(type(c))
+
+# x = int(1)
+# y = float(1)
+# z = complex(1)
+
+# print(x)
+# print(x)
+# print(x)
+
+# operator types
+
+# a = 5
+# b = 7
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+# print(a//b)
+
+# a = 32
+# b = 30
+
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+# print(a >= b)
+# print(a <= b)
+
+# x = 10
+
+# if x < 30 and x > 30:
+    # print("yes")
+# else:
+    # print("no")
+
+# if x < 30 or x > 30:
+    # print("yes")
+# else:
+    # print("no")
+
+# if not (x < 30 or x > 30):
+    # print("yes")
+# else:
+    # print("no")
+
+# x = 10
+# y = 100
+# print(x is y)
+# print(x is not y)
+
+# a = ["bolero", "fortuner", "thar", "swift"]
+# b = "thar"
+# print(b in a)
+# print(b not in a)
+
+# print(10 & 5)
+
+# x = 500
+# y = 300
+
+# if x > y:
+    # print(f" {x} is greater")
+# elif x == y:
+    # print(f"{x} is equal to {y}")
+# else:
+    # print(f"{y} is greater") 
+
+# mytext = "my friend"
+# for n in mytext:
+    # print(n)
+# myname = "manikandan"
+# for char in myname:
+    # print(char)
+
+# i = 5
+# while i < 20:
+    # print(i)
+    # i += 1
+
+# mystring = "      i. like. cars. also. driving      "
+# print(mystring. upper())
+# print(mystring.lower())
+# print(mystring.strip())
+# print(len(mystring))
+# newlist = mystring.split(".")
+# print(len(newlist))
+
+# mytext = "hello brother"
+# print(mytext.replace("l","e"))
+
+# a = "hello"
+# b = "machi"
+
+# x = 5
+# y = 2
+# print(a + b)
+# print(x + y)
+# print(a+" "+b)
+# print(a, b)
+
+# name = "vijaya"
+# age = 58
+# height = 7.5
+
+# print(f"my mother name is {name} and his age is {age}")
+# print(f"my mother name is {name} and mymother age is next year will be {age+1}")
+# print(f"my mother name is {name}, his age is {age} and his height is {height}")
+
+# num= 25
+# num = int(input())
+# if num > 0:
+    # print("possitive")
+# elif num==0:
+    # print("zero")
+# else:
+    # print("negative")
+
+# num = 61
+# num = int(input())
+# if num%2 == 0:
+    # print("even number")
+# else:
+    # print("odd number")
+
+# mystring = "save the nature"
+# for char in  mystring:
+    # print(char)
+    # if char == "u":
+        # break
+
+# i=1
+# while i < 5:
+    # print(i)
+    # if i <= 5:
+        # break
+    # i += 1
+
+# for i in range (10):
+    # if i == 7:
+        # continue
+    # print(i)
+
+# i = 0
+# while i < 8:
+    # i+=1
+    # if i == 3:
+        # continue
+    # print(i)
+
+# for char in range (1):
+    # pass
+
+# for x in range (2, 10, 2):
+    # if x == 6:
+      # continue
+    # print (x)
+
+# adj =["hot", "cool", "normal"]
+# food = ["rice", "coffee", "chicken","drinks"]
+# for x in adj:
+    # for y in food:
+        # print(x, y)
+
+# for i in range(2,4):
+    # for j in range(1,11):
+        # print( i, "+", j, "=", i+j)
